c72c3aa34541ff2c4d0b3817aea4fced88a059a3
gillsinaglass <rdgill95@gmail.com>
2019-04-07 17:51:21 -0400

    added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,14 @@ import "./App.css";
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import logo from "./logo.svg"
+import NavBar from "./Components/NavBar"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <NavBar />
         </header>
       </div>
     );
